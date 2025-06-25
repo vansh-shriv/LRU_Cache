9cbf5e23bcd15b4df479c08104cdf6b18f6a7c8b
vansh-shriv <vanshsvst@gmail.com>
2025-06-25 23:45:18 +0530

    PORT Fixing

--- app.py
+++ app.py
@@ -49,6 +49,7 @@ def  get_cache_contents():
     return contents
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use Render's assigned port or default to 5000
+    app.run(host="0.0.0.0", port=port)
 
     
